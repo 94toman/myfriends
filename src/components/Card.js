b5ed546e3d88d00f0cf3d835d6583dd2d853acb8
Martin Toman <94toman@gmail.com>
2022-03-11 11:35:44 +0100

    random images generating

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import GetRandomImage from './GetRandomImage';
 
 // A single card of a friend with profile picture, name and email.
+
+// https://github.com/ozgrozer/100k-faces
+
+
 
 const Card = ({id, name, email}) => {
   return (
     <div className='tc w-20 br2 grow dib pa3 ma2 bw2 shadow-5 bg-white'>
-      <img src={`https://ozgrozer.github.io/100k-faces/0/1/00107${id}.jpg`} alt="friend" />
+      <GetRandomImage />
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/Searchbox';
+import 'tachyons';
+
+
+class App extends Component {            // It is a Smart component (it has a state), therefore the class syntax
+  constructor() {
+    super()                              // This just has to be there
+    this.state = {                       // Creates a Component state to perform communication between two sub-components
+      friendsList: [],              
+      searchfieldContent: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://gorest.co.in/public/v2/users')
+    .then(response => response.json())
+    .then(users => this.setState ({friendsList: users}))
+  }
+
+  onSearchChange = (event) => {          // It is not part of React, therefore the Arrow function. This function is passed to SearchBox component, it is triggered when SearchBox is changed.
+    this.setState({ searchfieldContent: event.target.value })        // This assigns the value of SearchBox into State of the App Component
+  }
+
+  render() {
+    const { friendList, searchfieldContent} = this.state;
+    const filteredFriends = friendsList.filter(friend => {             // A function that creates a new list of friends containing the Searchbox content
+      return friend.name.toLowerCase().includes(searchfieldContent.toLowerCase())
+    })
+    if (!friendsList.length) {
+      return <h1 className='tc'>LOADING</h1>
+    } else 
+      return (               // Whole website content is rendered here
+      <div className='tc'>
+        <h1>MyFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList friends={filteredFriends}/>
+      </div>
+    );
+  }
+}
+
+export default App;
