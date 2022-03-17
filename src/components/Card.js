9f379bb025a13d6b7d91083d0ed07bd21d9fb633
Martin Toman <94toman@gmail.com>
2022-03-17 11:23:07 +0100

    random image generator working

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import loading from './loading.gif';
 
 // A single card of a friend with profile picture, name and email.
 
 // https://github.com/ozgrozer/100k-faces
 
+// Alternative image without Promise: <img id={`photo${id}`} src={`https://ozgrozer.github.io/100k-faces/0/${Number(String(id)[0])}/00${id}.jpg`} alt='friend' />  
+
 const Card = ({id, name, email, url}) => {
+
   return (
     <div className='tc w-20 br2 grow dib pa3 ma2 bw2 shadow-5 bg-white'>
-      <img id={`photo${id}${url}`} src={`https://ozgrozer.github.io/100k-faces/0/${Number(String(id)[0])}/00${id}.jpg`} alt='friend' />  
-      <p>{url}</p>
+      <img src={url} alt='friend picture'/>
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,6 @@ import SearchBox from '../components/Searchbox';
 import Sticky from '../components/Sticky'
 import 'tachyons';
 
-
-
 class App extends Component {            // It is a Smart component (it has a state), therefore the class syntax
   constructor() {
     super()                              // This just has to be there
@@ -29,20 +27,16 @@ class App extends Component {            // It is a Smart component (it has a st
       }
     }
 
-    fetch('https://gorest.co.in/public/v2/users')
-    .then(response => response.json())
-    .then(users => this.setState ({friendsList: users}))
-    .then(() => {
-      this.state.friendsList.map((friend, i) => {
-        getImgUrl()
-        .then(data => (friend.url = data.url))
-    })}   
-    )
-    .then(() => {
-
-      console.log(this.state.friendsList)
+  fetch('https://gorest.co.in/public/v2/users')
+  .then(response => response.json())
+  .then(array => {
+    array.map((friend, i) => {
+      getImgUrl()
+      .then(data => (friend.url = data.url))
+      .then(() => this.setState ({friendsList: array}))
     })
-  } 
+  })
+} 
 
   onSearchChange = (event) => {          // It is not part of React, therefore the Arrow function. This function is passed to SearchBox component, it is triggered when SearchBox is changed.
     this.setState({ searchfieldContent: event.target.value })        // This assigns the value of SearchBox into State of the App Component
